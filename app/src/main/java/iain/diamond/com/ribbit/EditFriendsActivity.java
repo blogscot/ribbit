92b53df688bc09610e2d18da75573173ef28e81d
blogscot <iain.diamond@gmail.com>
2015-07-01 12:27:14 +0100

    Implement Friends Activity/Fragment

--- app/src/main/java/iain/diamond/com/ribbit/EditFriendsActivity.java
+++ app/src/main/java/iain/diamond/com/ribbit/EditFriendsActivity.java
@@ -1,33 +1,52 @@
 package iain.diamond.com.ribbit;
 
-import android.app.AlertDialog;
-import android.support.v7.app.ActionBarActivity;
+import android.app.ActionBar;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
+import com.parse.ParseRelation;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
-public class EditFriendsActivity extends ActionBarActivity {
+public class EditFriendsActivity extends ListActivity {
 
   public static final String TAG = EditFriendsActivity.class.getSimpleName();
+
+  protected List<ParseUser> mUsers;
+  protected ParseRelation<ParseUser> mFriendsRelation;
+  protected ParseUser mCurrentUser;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_edit_friends);
+
+    // TODO: Figure out how to set up the action bar.
+
+    getListView().setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
   }
 
   @Override
   protected void onResume() {
     super.onResume();
 
+    mCurrentUser = ParseUser.getCurrentUser();
+    mFriendsRelation = mCurrentUser.getRelation(ParseUtils.KEY_FRIENDS_RELATION);
+
+    // Display the user's friends
     ParseQuery<ParseUser> query = ParseUser.getQuery();
     query.orderByAscending(ParseUtils.KEY_USERNAME);
     query.setLimit(1000);
@@ -36,8 +55,40 @@ public class EditFriendsActivity extends ActionBarActivity {
       public void done(List<ParseUser> list, ParseException e) {
         if (e == null) {
           // success
+          mUsers = list;
+          ArrayList<String> usernames = new ArrayList<>();
+          for (ParseUser user : list) {
+            usernames.add(user.getUsername());
+          }
+          ArrayAdapter<String> adapter =
+                  new ArrayAdapter<String>(EditFriendsActivity.this,
+                          android.R.layout.simple_list_item_checked,
+                          usernames);
+          setListAdapter(adapter);
+          addFriendCheckMarks();
         } else {
-          ParseUtils.displayMessage(EditFriendsActivity.this, "Error", "Problem communicating with server");
+          ParseUtils.displayMessage(EditFriendsActivity.this, getString(R.string.generic_error_message), getString(R.string.server_commication_error));
+        }
+      }
+    });
+  }
+
+  private void addFriendCheckMarks() {
+    mFriendsRelation.getQuery().findInBackground(new FindCallback<ParseUser>() {
+      @Override
+      public void done(List<ParseUser> friends, ParseException e) {
+        if (e == null) {
+          // friends list returned - look for match
+          for (int i = 0; i < mUsers.size(); i++) {
+            ParseUser user = mUsers.get(i);
+            for (ParseUser friend: friends) {
+              if (friend.getObjectId().equals(user.getObjectId())) {
+                getListView().setItemChecked(i, true);
+              }
+            }
+          }
+        } else {
+          Log.e(TAG, e.getMessage());
         }
       }
     });
@@ -63,5 +114,28 @@ public class EditFriendsActivity extends ActionBarActivity {
     }
 
     return super.onOptionsItemSelected(item);
+  }
+
+  @Override
+  protected void onListItemClick(ListView l, View v, int position, long id) {
+    super.onListItemClick(l, v, position, id);
+
+    // Add clicked user as a friend
+    if (getListView().isItemChecked(position)) {
+      // add friend
+      mFriendsRelation.add(mUsers.get(position));
+    }
+    else {
+      // remove friend
+      mFriendsRelation.remove(mUsers.get(position));
+    }
+    mCurrentUser.saveInBackground(new SaveCallback() {
+      @Override
+      public void done(ParseException e) {
+        if (e != null) {
+          Log.e(TAG, e.getMessage());
+        }
+      }
+    });
   }
 }
--- app/src/main/java/iain/diamond/com/ribbit/FriendsFragment.java
+++ app/src/main/java/iain/diamond/com/ribbit/FriendsFragment.java
@@ -3,15 +3,63 @@ package iain.diamond.com.ribbit;
 
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseRelation;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FriendsFragment extends ListFragment {
+
+  private static final String TAG = FriendsFragment.class.getSimpleName();
+
+  protected ParseRelation<ParseUser> mFriendsRelation;
+  protected ParseUser mCurrentUser;
+  protected List<ParseUser> mFriends;
+
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
     View rootView = inflater.inflate(R.layout.fragment_friends, container, false);
 
     return rootView;
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+
+    mCurrentUser = ParseUser.getCurrentUser();
+    mFriendsRelation = mCurrentUser.getRelation(ParseUtils.KEY_FRIENDS_RELATION);
+
+    // Update fragment with the user's list of friends
+    mFriendsRelation.getQuery()
+            .addDescendingOrder(ParseUtils.KEY_USERNAME)
+            .findInBackground(new FindCallback<ParseUser>() {
+      @Override
+      public void done(List<ParseUser> friends, ParseException e) {
+        if (e == null) {
+          mFriends = friends;
+          ArrayList<String> usernames = new ArrayList<>();
+          for (ParseUser user : friends) {
+            usernames.add(user.getUsername());
+          }
+          ArrayAdapter<String> adapter =
+                  new ArrayAdapter<String>(getListView().getContext(),
+                          android.R.layout.simple_list_item_1,
+                          usernames);
+          setListAdapter(adapter);
+        } else {
+          ParseUtils.displayMessage(getListView().getContext(), TAG, e.getMessage());
+        }
+      }
+    });
   }
 }
